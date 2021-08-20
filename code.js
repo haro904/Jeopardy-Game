f78b80f5bbcd60ccb3ca36e3829fb9784eee9e14
haro904 <hannah.glover@yahoo.comgit config --global pull.rebase true$ git config --global user.email hannah.glover@yahoo.com>
2021-08-20 11:16:34 -0400

    I finished the base code to get the game running

--- code.js
+++ code.js
@@ -1,4 +1,68 @@
-// To run this assignment, right click on index.html in the Visual Studio Code file explorer to the left
-// and select "Open with Live Server"
+//Global variables
+let cluesArray;
+let mainTag = document.querySelector("main");
+let h3Tag = document.querySelector("h3");
+let gameButton = document.getElementById("game-button");
+let currentScore = 0;
+let clueIndex;
+let currentClue;  
+let answerBox = document.createElement("INPUT");
+let scoreBoard = document.createElement("div");
 
-// YOUR CODE HERE!
+//Asynchronous code
+function startGame ()  {
+    fetch("https://jservice.io/api/random")
+    .then((response) => response.json())
+    .then((data) => fetch("https://jservice.io/api/clues?category="+data[0].category_id)
+    .then((response) => response.json())
+    .then((data) => {
+        // console.log(data)
+        cluesArray = data;
+        displayQuestion(cluesArray)
+    }))
+}
+
+//Global functions Alphabetized
+function displayQuestion(data) {
+    
+    scoreBoard.innerText = `Current Score: ${currentScore}`
+    mainTag.append(scoreBoard);
+
+    clueIndex = Math.floor(Math.random() * data.length);
+     currentClue = data[clueIndex];
+     h3Tag.innerText = currentClue.question;
+     console.log(currentClue.answer);
+     cluesArray.splice(clueIndex, 1);
+    
+
+    
+    answerBox.setAttribute("type", "text");
+    answerBox.setAttribute("placeholder", "your answer?"),
+    mainTag.append(answerBox);
+
+    gameButton.innerText = "Submit";
+};
+
+function eventHandler() {
+    if (gameButton.innerText == "Submit") {
+        
+        if (answerBox.value.toLowerCase() == currentClue.answer.toLowerCase()) {
+            currentScore ++;
+            scoreBoard.innerText = `Current Score: ${currentScore}`
+            
+            clueIndex = Math.floor(Math.random() * cluesArray.length);
+            currentClue = cluesArray[clueIndex];
+            console.log(currentClue.answer);
+            
+            h3Tag.innerText = currentClue.question;
+            cluesArray.splice(clueIndex, 1);
+        } else {
+            currentScore = 0;
+            h3Tag.innerText = "That was incorrect!";
+            gameButton.innerText = "Start New Game?";
+        }
+    } else {
+        startGame()
+    }
+}
+gameButton.addEventListener("click", eventHandler);
